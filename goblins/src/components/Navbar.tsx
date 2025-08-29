32237d6fadc2cd2ca600ac3d840d17046fa5be5f
mf <michaelf873@icloud.com>
2025-08-29 22:51:08 +1000

    added shadcn for navbar

---
+++ goblins/src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
+
+
+<NavigationMenu>
+  <NavigationMenuList>
+    <NavigationMenuItem>
+      <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+      <NavigationMenuContent>
+        <NavigationMenuLink>Link</NavigationMenuLink>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+  </NavigationMenuList>
+</NavigationMenu>
+
+
+
+
+//https://ui.shadcn.com/docs/components/navigation-menu
